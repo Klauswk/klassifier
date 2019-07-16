0a8b9e40c2455f489b115597227252c4d5257fa7
Klaus Klein <klaus.klein@cwi.com.br>
2019-07-16 15:48:33 -0300

    Base version created

---
+++ main.cpp
@@ -0,0 +1,204 @@
+#include <string>
+#include <iostream>
+#include <dirent.h>
+#include <stdio.h>
+#include <regex>
+#ifdef WIN32
+#include <direct.h>
+#define GetCurrentDir _getcwd
+#else
+#include <unistd.h>
+#include <sys/stat.h>
+#define GetCurrentDir getcwd
+#endif
+#include <list>
+using namespace std;
+
+vector<string> arrayOfDirectories = {"Music",
+                                     "Videos",
+                                     "Pictures",
+                                     "Archives",
+                                     "Documents",
+                                     "Books",
+                                     "DEBPackages",
+                                     "Programs",
+                                     "RPMPackages"};
+
+bool regexOk(const string &str, const regex &expression)
+{
+    if (regex_search(str, expression))
+    {
+        return true;
+    }
+    return false;
+}
+
+void make_directory(const char *name)
+{
+#ifdef WIN32
+    _mkdir(name);
+#else
+    mkdir(name, 777);
+#endif
+}
+
+list<string> listarArquivos()
+{
+    list<string> listaArquivos;
+    DIR *dir;
+    struct dirent *ent;
+    if ((dir = opendir(GetCurrentDir(0, 0))) != NULL)
+    {
+        while ((ent = readdir(dir)) != NULL)
+        {
+            listaArquivos.push_back(string(ent->d_name));
+        }
+        closedir(dir);
+    }
+    else
+    {
+        perror("");
+    }
+
+    return listaArquivos;
+}
+
+void checkOrCreateFolder(string folder)
+{
+    if (access(folder.c_str(), F_OK) == -1)
+    {
+        make_directory(folder.c_str());
+    }
+}
+
+bool some(string extension, std::initializer_list<string> list)
+{
+    for (string elem : list)
+    {
+        if (elem == extension)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+string getFolder(string fileName)
+{
+
+    int pos = fileName.rfind('.');
+
+    if (pos == -1)
+    {
+        return "";
+    }
+
+    string extension = fileName.substr(pos);
+    string folder;
+
+    if (some(extension, {".exe", ".msi"}))
+    {
+        folder = "Programs";
+    }
+    else if (some(extension, {".mp3", ".aac", ".flac", ".ogg", ".wma", ".m4a", ".aiff", ".wav", ".amr"}))
+    {
+        folder = "Music";
+    }
+    else if (some(extension, {".flv", ".ogv", ".avi", ".mp4", ".mpg", ".mpeg", ".3gp", ".mkv", ".ts", ".webm", ".vob", ".wmv"}))
+    {
+        folder = "Videos";
+    }
+    else if (some(extension, {".png", ".jpeg", ".gif", ".jpg", ".bmp", ".svg", ".webp", ".psd", ".tiff"}))
+    {
+        folder = "Pictures";
+    }
+    else if (some(extension, {".rar", ".zip", ".7z", ".gz", ".bz2", ".tar", ".dmg", ".tgz", ".xz", ".iso", ".cpio", ".deb", ".rpm"}))
+    {
+        folder = "Archives";
+    }
+    else if (some(extension, {".txt", ".pdf", ".doc", ".docx", ".odf", ".xls", ".xlsx", ".ppt", ".ptx", ".ppsx", ".odt", ".ods", ".md", ".json", ".csv"}))
+    {
+        folder = "Documents";
+    }
+
+    checkOrCreateFolder(folder);
+
+    return folder;
+}
+
+bool checkForIgnoredFilenames(string fileName)
+{
+    for (string directory : arrayOfDirectories)
+    {
+        if (directory == fileName)
+        {
+            return true;
+        }
+    }
+
+    if (fileName.front() == '.')
+        return true;
+
+    return false;
+}
+
+void move_file(string fileName)
+{
+
+    if (checkForIgnoredFilenames(fileName))
+    {
+        return;
+    }
+
+    std::cout << endl
+              << fileName;
+
+#ifdef WIN32
+    GetFullPathName(fileName);
+#else
+    string folder = getFolder(fileName);
+
+    if (folder.size() < 1)
+    {
+        return;
+    }
+    cout << folder << endl;
+
+    char *fileLocation = realpath(fileName.c_str(), NULL);
+    cout << " - " << fileLocation;
+
+    char *folderLocation = realpath(folder.c_str(), NULL);
+    cout << " - " << folderLocation;
+
+    string newLocation(folderLocation);
+
+    newLocation = newLocation + "/" + fileName;
+
+    cout << " - " << newLocation << endl;
+
+    int OK = rename(fileLocation, newLocation.c_str());
+
+    if(OK != 0) {
+        cout << "An error occour:" << endl << errno << endl; 
+    }
+#endif
+}
+
+int main()
+{
+    list<string> listaArquivos = listarArquivos();
+
+    for (std::list<string>::iterator it = listaArquivos.begin(); it != listaArquivos.end(); ++it)
+        move_file(*it);
+
+    /*make_directory("Music");
+    make_directory("Videos");
+    make_directory("Pictures");
+    make_directory("Archives");
+    make_directory("Documents");
+    make_directory("Books");
+    make_directory("DEBPackages");
+    make_directory("Programs");
+    make_directory("RPMPackages");*/
+}
